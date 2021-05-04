8d5818f848d4f4ad43d6a7d5f708946e611714f2
Felipe Schulz <Schulz.neo@gmail.com>
2021-05-04 15:31:41 -0300

    Initial commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Linq;
+using Shop.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Shop.Services;
+using Shop.Repositories;
+
+
+namespace Shop.Controllers
+{
+    [Route("v1/account")]
+    public class HomeController : ControllerBase
+    {
+        [HttpPost]
+        [Route("login")]
+        [AllowAnonymous]
+        public async Task<ActionResult<dynamic>> Authenticate([FromBody] User model)
+        {
+            var user = UserRepository.Get(model.Username, model.Password);
+
+            if (user == null)
+                return NotFound(new { message = "Usuário ou senha inválidos" });
+
+            var token = TokenService.GenerateToken(user);
+            user.Password = "";
+            return new
+            {
+                user = user,
+                token = token
+            };
+        }
+    }
+}
---
+++ Settings.cs
@@ -0,0 +1,7 @@
+namespace Shop
+{
+    public static class Settings
+    {
+        public static string Secret = "hash456ffdgh67e8887hj0flds34sss67da3sd";
+    }
+}
